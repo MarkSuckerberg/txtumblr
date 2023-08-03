4a71aa249b4502e19f5643a74eef84f8758beda6
Mark Suckerberg <mark@suckerberg.gay>
2023-08-02 21:06:04 -0500

    Initial working commit

---
+++ src/index.test.ts
@@ -0,0 +1,41 @@
+import { unstable_dev } from 'wrangler';
+import type { UnstableDevWorker } from 'wrangler';
+import { describe, expect, it, beforeAll, afterAll } from 'vitest';
+
+describe('Worker', () => {
+	let worker: UnstableDevWorker;
+
+	beforeAll(async () => {
+		worker = await unstable_dev('src/index.ts');
+	});
+
+	afterAll(async () => {
+		await worker.stop();
+	});
+
+	it('should return 200 response with a text opengraph tag',async () => {
+		const req = new Request(
+			'https://fxtumblr.suckerberg.workers.dev/engineering/690135035533230080',
+			{ method: 'GET' }
+		);
+		const resp = await worker.fetch(req.url);
+		expect(resp.status).toBe(200);
+
+
+		const text = await resp.text();
+		expect(text).toContain(`<meta property="og:description" content="Making GIFs load faster`)
+	})
+
+	it('should return 200 response with an image opengraph tag', async () => {
+		const req = new Request(
+			'https://fxtumblr.suckerberg.workers.dev/engineering/713599421825351680',
+			{ method: 'GET' }
+		);
+		const resp = await worker.fetch(req.url);
+		expect(resp.status).toBe(200);
+
+
+		const text = await resp.text();
+		expect(text).toContain(`<meta property="og:image" content="https://64.media.tumblr.com/0807dffbcf1e9db8a3ddf31b4f026c39/dec349afc8bbeb99-9a/s2048x3072/a10138f06f43173037f613167f32acc41495996c.png" />`)
+	});
+});
---
+++ src/index.ts
@@ -0,0 +1,151 @@
+import {
+	TumblrBlocksPost,
+	TumblrNeueContentBlock,
+	TumblrNeueImageBlock,
+	TumblrNeueTextBlock,
+	TumblrNeueVideoBlock,
+} from 'typeble';
+
+interface TumblrBotEnv {
+	TUMBLR_CONSUMER_KEY: string;
+}
+
+export default {
+	async fetch(request: Request, env: TumblrBotEnv) {
+		const url = new URL(request.url);
+		const pathInfo = url.pathname.split('/');
+		const trimmedPathInfo = pathInfo.filter(string => string);
+		const username = trimmedPathInfo[0];
+		const postID = trimmedPathInfo[1];
+
+		if (!Number.isInteger(+postID)) {
+			return new Response('Bad post ID', { status: 400 });
+		}
+
+		if (!username) {
+			return new Response('No username provided', { status: 400 });
+		}
+
+		//TODO: Make this properly use typeble
+		const postResponse = await fetch(
+			`https://www.tumblr.com/api/v2/blog/${username}/posts?api_key=${env.TUMBLR_CONSUMER_KEY}&id=${postID}&npf=true`
+		);
+		const postData: any = await postResponse.json();
+
+		if (!postData['response']['posts']) {
+			return new Response('Post not found', { status: 404 });
+		}
+
+		const post = postData['response']['posts'][0] as TumblrBlocksPost;
+		const originalPost = post.trail[0] as TumblrBlocksPost;
+
+		if (url.searchParams.has('oembed')) {
+			return oembed(post);
+		} else if (url.searchParams.has('json')) {
+			return new Response(JSON.stringify(post), {
+				headers: {
+					'content-type': 'text/json;charset=UTF-8',
+				},
+			});
+		} else {
+			return mainPage(post, originalPost, url);
+		}
+	},
+};
+
+function oembed(post: TumblrBlocksPost): Response {
+	const response = {
+		author_name: `${post.note_count} 📝`,
+		author_url: post.blog.url,
+		provider_name: 'fxTumblr',
+		provider_url: '',
+		title: 'Tumblr',
+		type: 'link',
+		version: '1.0',
+	};
+
+	return new Response(JSON.stringify(response), {
+		headers: {
+			'content-type': 'text/json;charset=UTF-8',
+		},
+	});
+}
+
+function mainPage(
+	post: TumblrBlocksPost,
+	originalPost: TumblrBlocksPost | undefined,
+	url: URL
+): Response {
+	const blocks = originalPost ? originalPost.content.concat(post.content) : post.content;
+
+	const textBlocks = blocks.filter(element => element.type == 'text') as TumblrNeueTextBlock[];
+	const text = textBlocks.map(block => block.text).join('\n\n');
+
+	const imageBlocks = blocks.filter(element => element.type == 'image') as TumblrNeueImageBlock[];
+	const imageMediaObjects = imageBlocks.map(
+		block => block.media.find(media => media.has_original_dimensions) || block.media[0]
+	);
+
+	const imageTags = imageMediaObjects.map(
+		mediaObject =>
+			`<meta property="og:image" content="${mediaObject.url}" />
+				<meta property="og:image-height" content="${mediaObject.height}" />
+				<meta property="og:image-width" content="${mediaObject.width}" />
+				<meta property="twitter:image" content="${mediaObject.url}" />`
+	);
+
+	const imageIndex = url.searchParams.get('image');
+	const imagesToShow = imageIndex ? imageTags[+imageIndex] : imageTags.join('\n');
+
+	const title = `${post.blog.name} ${
+		originalPost ? `🔁 ${originalPost.blog.name}` : `(${post.blog.title})`
+	}`;
+
+	console.log(title);
+
+	const html = `<!DOCTYPE html>
+	<head>
+		<title>${title}</title>
+		<meta name="description" content="${text}" />
+		<link rel="canonical" href="${post.post_url} />
+
+		<!-- OpenGraph embed tags -->
+		<meta property="og:site_name" content="fxTumblr Beta" />
+		<meta property="og:type" content="website" />
+		<meta property="og:title" content="${title}" />
+		<meta property="og:url" content="${post.post_url}" />
+		<meta property="og:description" content="${text}" />
+
+		<!-- Twitter embed tags -->
+		<meta name="twitter:card" content="summary_large_image">
+		<meta property="twitter:domain" content="tumblr.com">
+		<meta property="twitter:title" content="${title}" />
+		<meta property="twitter:creator" content="${post.blog_name}" />
+		<meta property="twitter:site" content="${post.blog.url}" />
+		<meta property="twitter:url" content="${post.post_url}" />
+		<meta property="twitter:description" content="${text}" />
+
+		${imagesToShow}
+
+		<link
+			rel="alternate"
+			href="${url.protocol}//${url.hostname}${url.pathname}?oembed"
+			type="application/json+oembed"
+			title="${post.blog_name}"
+		/>
+
+		${
+			!url.searchParams.has('noRedirect')
+				? `<meta http-equiv="refresh" content="0;url=${post.post_url}" />`
+				: ''
+		}
+
+		<meta property="theme-color" content="#${imageBlocks.find(block => block.colors)?.colors?.c0}" />
+	</head>`;
+
+	return new Response(html, {
+		headers: {
+			'content-type': 'text/html;charset=UTF-8',
+		},
+	});
+}
